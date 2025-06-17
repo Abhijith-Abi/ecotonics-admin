4dc31d7cf015d27cea123a10f672b67447987490
Abhijith-Abi <abhijithabhijith1999@gmail.com>
2025-06-18 00:51:09 +0530

    feat: enhance sidebar with collapsible menu and badges for items

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -18,8 +18,10 @@ import {
     SidebarGroupLabel,
     SidebarHeader,
     SidebarMenu,
+    SidebarMenuBadge,
     SidebarMenuButton,
     SidebarMenuItem,
+    SidebarMenuSub,
     SidebarSeparator,
 } from "./ui/sidebar";
 import Link from "next/link";
@@ -30,6 +32,11 @@ import {
 } from "@radix-ui/react-dropdown-menu";
 import { DropdownMenuContent, DropdownMenuItem } from "./ui/dropdown-menu";
 import logo from "../../public/assets/images/logo.svg";
+import {
+    Collapsible,
+    CollapsibleContent,
+    CollapsibleTrigger,
+} from "./ui/collapsible";
 
 function AppSidebar() {
     const items = [
@@ -42,16 +49,30 @@ function AppSidebar() {
             title: "Services",
             url: "#",
             icon: Inbox,
+            children: [
+                {
+                    title: "Service 1",
+                    url: "#service1",
+                    icon: Inbox,
+                },
+                {
+                    title: "Service 2",
+                    url: "#service2",
+                    icon: Inbox,
+                },
+            ],
         },
         {
             title: "Workforce",
             url: "#",
             icon: Calendar,
+            badge: "14",
         },
         {
             title: "Customers",
             url: "#",
             icon: Users,
+            badge: "200",
         },
         {
             title: "Masters",
@@ -85,16 +106,68 @@ function AppSidebar() {
                     <SidebarGroupLabel>Menus</SidebarGroupLabel>
                     <SidebarGroupContent>
                         <SidebarMenu>
-                            {items.map((item) => (
-                                <SidebarMenuItem key={item.title}>
-                                    <SidebarMenuButton asChild>
-                                        <a href={item.url}>
-                                            <item.icon />
-                                            <span>{item.title}</span>
-                                        </a>
-                                    </SidebarMenuButton>
-                                </SidebarMenuItem>
-                            ))}
+                            {items.map((item) =>
+                                item.children ? (
+                                    <Collapsible
+                                        key={item.title}
+                                        className="group/collapsible"
+                                    >
+                                        <SidebarMenuItem>
+                                            <CollapsibleTrigger asChild>
+                                                <SidebarMenuButton>
+                                                    <item.icon />
+                                                    <span>{item.title}</span>
+                                                    <ChevronUp className="ml-auto transition-transform group-data-[state=closed]/collapsible:rotate-180" />
+                                                </SidebarMenuButton>
+                                            </CollapsibleTrigger>
+                                            <CollapsibleContent>
+                                                <SidebarMenuSub>
+                                                    {item.children.map(
+                                                        (child) => (
+                                                            <SidebarMenuItem
+                                                                key={
+                                                                    child.title
+                                                                }
+                                                            >
+                                                                <SidebarMenuButton
+                                                                    asChild
+                                                                >
+                                                                    <a
+                                                                        href={
+                                                                            child.url
+                                                                        }
+                                                                    >
+                                                                        <child.icon />
+                                                                        <span>
+                                                                            {
+                                                                                child.title
+                                                                            }
+                                                                        </span>
+                                                                    </a>
+                                                                </SidebarMenuButton>
+                                                            </SidebarMenuItem>
+                                                        )
+                                                    )}
+                                                </SidebarMenuSub>
+                                            </CollapsibleContent>
+                                        </SidebarMenuItem>
+                                    </Collapsible>
+                                ) : (
+                                    <SidebarMenuItem key={item.title}>
+                                        <SidebarMenuButton asChild>
+                                            <a href={item.url}>
+                                                <item.icon />
+                                                <span>{item.title}</span>
+                                            </a>
+                                        </SidebarMenuButton>
+                                        {item.badge && (
+                                            <SidebarMenuBadge>
+                                                {item.badge}
+                                            </SidebarMenuBadge>
+                                        )}
+                                    </SidebarMenuItem>
+                                )
+                            )}
                         </SidebarMenu>
                     </SidebarGroupContent>
                 </SidebarGroup>
@@ -105,7 +178,7 @@ function AppSidebar() {
                         <DropdownMenu>
                             <DropdownMenuTrigger asChild>
                                 <SidebarMenuButton>
-                                    <User /> Abi Admin{" "}
+                                    <User /> Abi Admin
                                     <ChevronUp className="ml-auto" />
                                 </SidebarMenuButton>
                             </DropdownMenuTrigger>
