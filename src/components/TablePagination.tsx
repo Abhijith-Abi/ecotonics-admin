52eddf236f664f9d7c134f03b75f007acb4ffb21
Abhijith-Abi <abhijithabhijith1999@gmail.com>
2025-06-20 12:54:07 +0530

    fix: adjust layout of DataTablePagination component for improved structure

--- src/components/TablePagination.tsx
+++ src/components/TablePagination.tsx
@@ -29,8 +29,6 @@ export function DataTablePagination<TData>({
             <div className="text-muted-foreground flex-1 text-sm">
                 {/* {table.getFilteredSelectedRowModel().rows.length} of{" "}
                 {table.getFilteredRowModel().rows.length} row(s) selected. */}
-            </div>
-            <div className="flex items-center space-x-6 lg:space-x-8">
                 <div className="flex items-center space-x-2">
                     <p className="text-sm font-medium">Pages</p>
                     <Select
@@ -58,6 +56,8 @@ export function DataTablePagination<TData>({
                         </SelectContent>
                     </Select>
                 </div>
+            </div>
+            <div className="flex items-center space-x-6 lg:space-x-8">
                 <div className="flex w-[100px] items-center justify-center text-sm font-medium">
                     Page {table.getState().pagination.pageIndex + 1} of{" "}
                     {table.getPageCount()}
